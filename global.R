20d51ef03506a1b861cf1321473005b7c4b12212
Christopher Teixeira <chris@christopherteixeira.com>
2021-08-16 10:35:04 -0400

    structured differently

--- global.R
+++ global.R
@@ -1,6 +1,16 @@
 # Used packages
-packages <- c("shiny", "shinydashboard", "shinythemes", "ffsimulator", "ggplot2", "ggthemes",
-            "lubridate", "ffscrapr", "DT", "dplyr", 'shinyjs', "patchwork")
+packages <- c("shiny", 
+              "shinydashboard", 
+              "shinythemes", 
+              "ffsimulator", 
+              "ggplot2", 
+              "ggthemes",
+              "lubridate", 
+              "ffscrapr", 
+              "DT", 
+              "dplyr", 
+              'shinyjs', 
+              "patchwork")
 
 # Run the following command to verify that the required packages are installed. If some package
 # is missing, it will be installed automatically
